fc7c8b58f8de2d83a1505941c24ee18d95ba8cc1
leticiamoreiram <leticia.m@gec.inatel.br>
2024-04-14 16:23:55 -0300

    elaboração do componente Chat

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Chat.css";
+import Manual from "../Manual/Manual"
+import manualButtonIcon from "../../images/manual-button-icon.png";
+import newChatButtonIcon from "../../images/new-chat-button-icon.png";
+import { useState } from "react";
+
+function Chat() {
+
+  const [showManual, setShowManual] = useState(false);
+
+  const openManual = () => {
+    setShowManual(true);
+  };
+
+  const closeManual = () => {
+    setShowManual(false);
+  };
+
+  return (
+    <div className="chat-container">
+      {showManual && <Manual onClose={closeManual} />}
+      <div className="chat-header">
+        <button className="chat-manual-button" onClick={openManual}>
+          <img src={manualButtonIcon} alt="Manual" />
+        </button>
+        <h1 className="chat-title">New Chat</h1>
+        <button className="chat-add-button">
+          <img src={newChatButtonIcon} alt="Add" />
+          </button>
+      </div>
+      <div className="chat-body">
+        <div className="chat-messages"></div>
+        <div className="chat-input">
+          <input type="text" placeholder="Write your question here" />
+          <button className="send-button">Send</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
