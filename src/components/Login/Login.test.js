6b1216f3a899125b3335258354e0a452708e3baa
PedroBorem <pedro.borem@outlook.com>
2024-05-08 16:16:46 -0300

    test: removendo testes de interface

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -8,21 +8,6 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('Login Component', () => {
-  /*
-  it('renders login form correctly', () => {
-    const { getByText, getByPlaceholderText } = render(<Login />);
-    expect(getByText(/Enter your/i)).toBeInTheDocument(); // Using a regular expression to match across multiple lines
-    expect(getByPlaceholderText('Login')).toBeInTheDocument();
-    expect(getByPlaceholderText('Password')).toBeInTheDocument();
-    expect(getByText('Next')).toBeInTheDocument();
-  });
-  
-  it('displays error message when fields are empty', () => {
-    const { getByText } = render(<Login />);
-    fireEvent.click(getByText('Next'));
-    expect(getByText(/Please fill in all fields/i)).toBeInTheDocument(); // Using a regular expression to match across multiple lines
-  });
-  */
   
   it('navigates to /chat when fields are filled', () => {
     const navigateMock = jest.fn();
