539733eb85862d1c53ba2889387e877158c6f526
alinepmarcondes <aline.patta@ges.inatel.br>
2024-05-09 23:48:15 -0300

    fix: ajuste da importacao

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Login.css';
 import { useNavigate } from "react-router-dom";
-import { validateInputs } from './utils/validation';
+import { validateInputs } from '../utils/validation';
 
 function Login() {
   const navigate = useNavigate();
