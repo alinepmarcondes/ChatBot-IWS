e1b6fb3e88ee206cbbe881146e420e0807317c73
alinepmarcondes <aline.patta@ges.inatel.br>
2024-05-09 23:47:28 -0300

    feat: add logica para validacao dos campos edit/create

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -2,19 +2,33 @@ import React, { useState } from 'react';
 import './Create.css';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+import { validateInputs } from '../utils/validation';
 
 function Create() {
   const navigate = useNavigate();
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleCreate = async () => {
-    try {
-      await axios.post('http://localhost:5000/users', { login: login, password: password, type: 'user' });
-      console.log('Usuário criado com sucesso!');
-    } catch (error) {
-      console.error('Erro ao criar usuário:', error);
-    } 
+    if (validateInputs(login, password, setErrorMessage)) {
+      try {
+        await axios.post('http://localhost:5000/users', { login: login, password: password, type: 'user' });
+        console.log('Usuário criado com sucesso!');
+      } catch (error) {
+        console.error('Erro ao criar usuário:', error);
+      } 
+    }  
+  };
+
+  const handleLoginChange = (e) => {
+    setLogin(e.target.value);
+    setErrorMessage('');
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setErrorMessage('');
   };
 
   const handleBackNewUser = () => {
@@ -24,8 +38,27 @@ function Create() {
   return (
     <div className="create-page">
       <h2 className="create-title">Create<br />  </h2>
-      <input type="text" placeholder=" Login " className="login-newuser-input" value={login} onChange={(e) => setLogin(e.target.value)} />
-      <input type="password" placeholder=" Password " className="password-newuser-input" value={password} onChange={(e) => setPassword(e.target.value)}  />
+      <input 
+        type="text" 
+        placeholder=" Login " 
+        className="login-newuser-input" 
+        value={login} 
+        onChange={(e) => {
+          setLogin(e.target.value);
+          handleLoginChange(e); 
+        }} 
+        />
+      <input 
+        type="password" 
+        placeholder=" Password " 
+        className="password-newuser-input" 
+        value={password} 
+        onChange={(e) => {
+          setPassword(e.target.value);
+          handlePasswordChange(e);
+        }}  
+      />
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
       <button className="create-newuser-button" onClick={handleCreate}>Create</button>
       <button className="back-create-button" onClick={handleBackNewUser}>Back</button>
     </div>
