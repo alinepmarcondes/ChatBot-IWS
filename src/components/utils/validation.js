98c04c13d54bab8b6f148258d9de9b3d7efe6a42
alinepmarcondes <aline.patta@ges.inatel.br>
2024-05-24 20:28:24 -0300

    feat: add validação para criar user

--- src/components/utils/validation.js
+++ src/components/utils/validation.js
@@ -5,4 +5,16 @@ export const validateInputs = (login, password, setErrorMessage) => {
     }
     return true;
   };
-  
+
+export function validateInputsType( password, setErrorMessage) {
+  if (password.length < 8) {
+    setErrorMessage('Password must be at least 8 characters long');
+    return false;
+  }
+  const passwordComplexityRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!passwordComplexityRegex.test(password)) {
+    setErrorMessage('Password must be stronger');
+    return false;
+  }
+  return true;
+}
