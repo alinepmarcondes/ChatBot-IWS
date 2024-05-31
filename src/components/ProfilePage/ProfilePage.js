436ecbfa80b3c3670d5383f9f3799c23d997f513
PedroBorem <pedro.borem@outlook.com>
2024-05-31 16:37:13 -0300

    substituicao pelos novos icons nas paginas

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import './ProfilePage.css';
-import closeButtonIcon from "../../images/close-button-icon.png";
-import profileIcon from "../../images/profile-icon.png";
-import goTo from "../../images/go-to-icon-2.png";
+import "./ProfilePage.css";
+import CloseButtonIcon from "../icons/closeButtonIcon";
+import ProfileIcon from "../icons/profileIcon";
+import GoToIcon from "../icons/goToIcon";
 import Historic from "../Historic/Historic";
 import { useNavigate } from "react-router-dom";
 
 let isAdmin = true;
 
 function ProfilePage({ onClose }) {
-  const [showManage] = useState(isAdmin); 
+  const [showManage] = useState(isAdmin);
   const [showHistoric, setShowHistoric] = useState(false);
   const navigate = useNavigate();
 
@@ -18,46 +18,50 @@ function ProfilePage({ onClose }) {
   };
 
   const handleManageToggle = () => {
-    navigate('/newuser');
+    navigate("/newuser");
   };
 
   const handleExit = () => {
-    navigate('/');
+    navigate("/");
   };
 
-  
   return (
     <div className="profile-page">
       <div className="profile-header">
         <button className="close-button" onClick={onClose}>
-          <img src={closeButtonIcon} alt="Fechar" />
+          <CloseButtonIcon className="close-button" />
         </button>
       </div>
 
       <div className="profile-content">
-        <button className="go-button">
-          <img src={profileIcon} alt="Profile" className="profile-icon" />
+        <button className="profile-button">
+          <ProfileIcon className="profile-icon" />
           <span className="hello-user">User</span>
         </button>
 
-        <div className="historic">
-          <span>Historic</span>
-          <button className="historic-button" onClick={handleHistoricToggle}>
-            <img src={goTo} alt="Historico" />
-          </button>
-        </div>
+        <ul className="profile-options">
+          <li className="option" onClick={handleHistoricToggle}>
+            <span>Historic</span>
+            <button className="option-button">
+              <GoToIcon />
+            </button>
+          </li>
 
-        <div className="manage" style={{ display: showManage ? 'block' : 'none' }}>
-          <span>Manage</span>
-          <button className="manage-button" onClick={handleManageToggle}>
-            <img src={goTo} alt="Manage" />
-          </button>
-        </div>
-        
+          {showManage && (
+            <li className="option" onClick={handleManageToggle}>
+              <span>Manage</span>
+              <button className="option-button">
+                <GoToIcon />
+              </button>
+            </li>
+          )}
+        </ul>
       </div>
 
       <div className="profile-footer">
-        <button className="exit-button" onClick={handleExit}>Exit</button>
+        <button className="exit-button" onClick={handleExit}>
+          Exit
+        </button>
       </div>
 
       {showHistoric && <Historic onClose={handleHistoricToggle} />}
